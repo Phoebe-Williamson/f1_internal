72979a3238902967f01da29cf23dfc1b39998003
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-09-05 09:17:07 +1200

    Create login.php

---
+++ login.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<?php
+	session_start();
+	include '../f1_mysqli.php';
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+		<title>
+			Formula 1 - Home
+		</title>
+	</head>
+
+	<body>
+		<header> 
+			<div class="logo">
+				<a class="two" href="home.php">
+            		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
+				</a>
+			</div>
+			<h1 class="title">
+				f1 - home
+			</h1>
+			<div class="search">
+				<h1>Search</h1>
+				<form method="post">
+					<input type="text" name="search">
+					<input type="submit" name="submit" value="Search" class="search_button">
+				</form>
+
+				<?php				
+					/* searches database to see if the input matches */
+					if (isset($_POST['search'])) {
+						$search = $_POST['search'];
+
+						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND Fname LIKE '%$search%'";
+						
+						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND Lname LIKE '%$search%'";
+						
+						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND DriverID LIKE '%$search%'";
+						
+						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND TeamName LIKE '%$search%'";
+						
+
+						$search_query_results = mysqli_query($conn, $search_query_fname);
+
+						$count = mysqli_num_rows($search_query_results);
+
+						/* checks if there are any results from the search */
+						if ($count == 0) {
+							echo "There were no search results!";
+						} else {
+							/* prints search results */
+							while ($row = mysqli_fetch_array($search_query_results)) {
+								echo "Driver and number: ";
+								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
+								echo "<br>";
+								echo "Team: ";
+								echo $row['TeamName'];
+								echo "<br>";
+							}
+						}
+					}
+							?>
+			
+			</div>
+			<div class="login">
+				login
+			</div>
+			<nav>
+				<a class="one" href="home.php">Home</a>
+				<a class="one" href="driver.php">Drivers</a>
+				<a class="one" href="teams.php"> Teams</a>
+				<a class="one" href="tracks.php"> Tracks</a>
+    			<link rel="stylesheet" type="text/css" href="styles.css">
+			</nav>
+		</header>
+		<?php
+		include '../f1_mysqli.php';
+
+		$password = 'Cheese';
+
+		$bcrypt_password = password_hash($password, PASSWORD_BCRYPT);
+
+		$user_types = 'Cheese';
+		
+		echo $bcrypt_password;
+
+		?>
+
+		<h2>Login Here </h2>
+		<form name='login_form' id='login+form' method='post' action='process_login.php'>
+		<label for='Username'>Username:</label>
+		<input type='text' name='Username' required><br>
+
+		<label for='Password'>Password:</label>
+		<input type='text' name='Password' required><br>
+
+		<input type='submit' name='submit' id='submit' value='Log In'>
+		</form>
+	</body>
+</html>
