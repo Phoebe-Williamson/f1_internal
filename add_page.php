725db5d1caeef9e058263bd5b0c0969336e7eafd
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-09-11 14:22:16 +1200

    Create add_page.php

---
+++ add_page.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<?php
+	session_start();
+	include '../f1_mysqli.php';
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+		<title>
+			Formula 1 - Add page
+		</title>
+	</head>
+
+	<body>
+		<header> 
+			<div class="logo">
+				<a class="two" href="home.php">
+            		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
+				</a>
+			</div>
+			<h1 class="title">
+				f1 - home
+			</h1>
+			<div class="search">
+				<h1>Search</h1>
+				<form method="post">
+					<input type="text" name="search">
+					<input type="submit" name="submit" value="Search" class="search_button">
+				</form>
+				<?php				
+					/* searches database to see if the input matches */
+					if (isset($_POST['search'])) {
+						$search = $_POST['search'];
+
+						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND Fname LIKE '%$search%'";
+						
+						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND Lname LIKE '%$search%'";
+						
+						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND DriverID LIKE '%$search%'";
+						
+						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
+											  FROM Bio b, Driver d, Team t
+											  WHERE b.BioID = d.BioID
+											  AND t.TeamID = d.TeamID
+											  AND TeamName LIKE '%$search%'";
+						
+
+						$search_query_result1 = mysqli_query($conn, $search_query_fname);
+						$search_query_result2 = mysqli_query($conn, $search_query_lname);
+						$search_query_result3 = mysqli_query($conn, $search_query_DriverID);
+						$search_query_result4 = mysqli_query($conn,$search_query_teamname);
+
+						$count = mysqli_num_rows($search_query_result1) + mysqli_num_rows($search_query_result2) + mysqli_num_rows($search_query_result3) +mysqli_num_rows($search_query_result4);
+
+						/* checks if there are any results from the search */
+						if ($count == 0) {
+							echo "There were no search results!";
+						} else {
+							/* prints search results */
+							while ($row = mysqli_fetch_array($search_query_result1)) {
+								echo "Driver and number: ";
+								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
+								echo "<br>";
+								echo "Team: ";
+								echo $row['TeamName'];
+								echo "<br>";
+							}
+						while ($row = mysqli_fetch_array($search_query_result2)) {
+								echo "Driver and number: ";
+								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
+								echo "<br>";
+								echo "Team: ";
+								echo $row['TeamName'];
+								echo "<br>";
+							}
+						while ($row = mysqli_fetch_array($search_query_result3)) {
+								echo "Driver and number: ";
+								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
+								echo "<br>";
+								echo "Team: ";
+								echo $row['TeamName'];
+								echo "<br>";
+							}
+						while ($row = mysqli_fetch_array($search_query_result4)) {
+								echo "Driver and number: ";
+								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
+								echo "<br>";
+								echo "Team: ";
+								echo $row['TeamName'];
+								echo "<br>";
+							}
+						}
+					}
+							?>
+			
+			</div>
+			<div class="login">
+				<a class="one" href="login.php">Login</a>
+				<a class="one" href="process_logout.php">Logout</a>
+			</div>
+			<nav>
+				<a class="one" href="home.php">Home</a>
+				<a class="one" href="driver.php">Drivers</a>
+				<a class="one" href="teams.php"> Teams</a>
+				<a class="one" href="tracks.php"> Tracks</a>
+    			<link rel="stylesheet" type="text/css" href="styles.css">
+			</nav>
+		</header>
