e0fe0f85dbc5e65bd671c6a89536376bad7d49d8
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-10-02 00:40:49 +1300

    commenting and code validation
    didnt like much of the code

--- race.php
+++ race.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
+<HTML lang="en">
 <?php
+/*starts the session and has databse connection so queries work*/
 	session_start();
 	include '../f1_mysqli.php';
 ?>
-<html>
 	<head>
+		<!-- Link to style sheet  -->
 		<link rel="stylesheet" href="styles.css">
+		<meta charset="utf-8">
+		<!-- Creates title -->
 		<title>
 			Formula 1 - Races
 		</title>
 	</head>
-
 	<body>
 		<header> 
 			<div class="logo">
-				<a class="two" href="home.php">
+				<!--Image acts as a link to the home page -->
+				<a class="two" href="home.ph">
             		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
 				</a>
 			</div>
+			<!-- Is the header section  -->
 			<h1 class="title">
-				Formula 1 - races
+				Formula 1 - Races
 			</h1>
+			<!-- Search bar section -->
 			<div class="search">
 				<h1>Search</h1>
+				<!-- Starts the search query -->
 				<form method="post">
 					<input type="text" name="search">
 					<input type="submit" name="submit" value="Search" class="search_button">
@@ -31,32 +38,35 @@
 					/* searches database to see if the input matches */
 					if (isset($_POST['search'])) {
 						$search = $_POST['search'];
-
+						/*Query based on search like first name*/
 						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Fname LIKE '%$search%'";
 						
+						/*Query based on search like last name*/
 						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Lname LIKE '%$search%'";
 						
+						/*Query based on search like driver number*/
 						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND DriverID LIKE '%$search%'";
 						
+						/*Query based on seach like team name*/
 						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND TeamName LIKE '%$search%'";
 						
-
+						/*turns queries into reusults*/
 						$search_query_result1 = mysqli_query($conn, $search_query_fname);
 						$search_query_result2 = mysqli_query($conn, $search_query_lname);
 						$search_query_result3 = mysqli_query($conn, $search_query_DriverID);
@@ -78,6 +88,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result2)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -86,6 +97,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result3)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -94,6 +106,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result4)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -104,16 +117,19 @@
 						}
 					}
 							?>
+			
 			</div>
+			<!-- Link to login page -->
 			<div class="login">
 				<?php
+					//checks to see if logged in
 					if((!isset($_SESSION['Logged_in'])) or $_SESSION['Logged_in'] != 1){
 					echo "Not logged in";
-					echo "<a class='one' href='login.php'>Login</a>";
+					echo "<a class='one' href='login.php'>Login</a>"; // displays if not logged in
 					}
 					else {
-					echo "Logged In: ".$_SESSION['Username'];
-					echo "<a class='one' href='process_logout.php'>Logout</a>";
+					echo "Logged In: ".$_SESSION['Username']; // shows username of who is logged in
+					echo "<a class='one' href='process_logout.php'>Logout</a>"; // dislpayed if loffed in
 					
 						$username = $_SESSION['Username'];
 						
@@ -125,35 +141,37 @@
 					$required_rank = "admin"; 
 						
 						if($user_rank == $required_rank){
-							echo "<a class='one' href='add_page.php'>Admin</a>";
+							echo "<a class='one' href='add_page.php'>Admin</a>"; // hsows admin tag if logged in
 						}	
 					}
 				?>
 			</div>
+			<!-- Nav bar links to all of the pages -->
 			<nav>
 				<a class="one" href="home.php">Home</a>
 				<a class="one" href="driver.php">Drivers</a>
 				<a class="one" href="teams.php"> Teams</a>
 				<a class="one" href="tracks.php"> Tracks</a>
-				<a class="one" href="race.php"> Races</a>
     			<link rel="stylesheet" type="text/css" href="styles.css">
 			</nav>
 		</header>
-		
+		<!--Starts grid-->
 		<div class="polaroid-gallery">
 				<?php
 					/* Gets info from database*/
-					$query = "SELECT r.RaceID, r.RaceName, r.DateTime, r.TrackID, r.DriverID from Race r, Track t, Driver d Where r.TrackID = t.TrackID AND r.DriverID = d.DriverID";
+					/*$query = "SELECT r.RaceID, r.RaceName, r.DateTime, r.TrackID, r.DriverID, t.Location from Race r, Track t, Driver d Where r.TrackID = t.TrackID AND r.DriverID = d.DriverID";*/
+					$query = "SELECT * FROM Race, Track Where Race.TrackID = Track.TrackID";
 					$result = mysqli_query($conn, $query);
 
 					/* checks that results are in database*/
 					while ($row = mysqli_fetch_assoc($result)) {
 						echo '<div class="polaroid">';
 						echo '<a class="three" href="race_profile.php?RaceID='.$row['RaceID'].'">';
-						echo "<input type='hidden' name='rID' value='" . $row['TrackID'] . "'>";
+						echo '<h1>';
+						echo $row['Location'];
+						echo '</h1>';
 						echo '<div class="caption">';
-						echo $row['RaceName']." - "; /* displays track name */
-						echo $row['DateTime']; /* displays track location in caption area */
+						echo $row['DateTime']; /* displays date and time of race in caption area */
 						echo '</div>';
 						echo '</div>';
 					}
