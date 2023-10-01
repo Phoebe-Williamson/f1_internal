29db6d559e6fb20b233707f239e88998f2396cf2
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-10-01 23:42:37 +1300

    commenting and validating code

--- delete_track.php
+++ delete_track.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
+<HTML lang="en">
 <?php
+/*starts the session and has databse connection so queries work*/
 	session_start();
 	include '../f1_mysqli.php';
 ?>
-<html>
 	<head>
+		<!-- Link to style sheet  -->
 		<link rel="stylesheet" href="styles.css">
+		<meta charset="utf-8">
+		<!-- Creates title -->
 		<title>
-			Formula 1 - Home
+			Formula 1 - Delete
 		</title>
 	</head>
-
 	<body>
 		<header> 
 			<div class="logo">
-				<a class="two" href="home.php">
+				<!--Image acts as a link to the home page -->
+				<a class="two" href="home.ph">
             		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
 				</a>
 			</div>
+			<!-- Is the header section  -->
 			<h1 class="title">
-				Formula 1 - Home
+				Formula 1 - Delete
 			</h1>
+			<!-- Search bar section -->
 			<div class="search">
 				<h1>Search</h1>
+				<!-- Starts the search query -->
 				<form method="post">
 					<input type="text" name="search">
 					<input type="submit" name="submit" value="Search" class="search_button">
@@ -31,32 +38,35 @@
 					/* searches database to see if the input matches */
 					if (isset($_POST['search'])) {
 						$search = $_POST['search'];
-
+						/*Query based on search like first name*/
 						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Fname LIKE '%$search%'";
 						
+						/*Query based on search like last name*/
 						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Lname LIKE '%$search%'";
 						
+						/*Query based on search like driver number*/
 						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND DriverID LIKE '%$search%'";
 						
+						/*Query based on seach like team name*/
 						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND TeamName LIKE '%$search%'";
 						
-
+						/*turns queries into reusults*/
 						$search_query_result1 = mysqli_query($conn, $search_query_fname);
 						$search_query_result2 = mysqli_query($conn, $search_query_lname);
 						$search_query_result3 = mysqli_query($conn, $search_query_DriverID);
@@ -78,6 +88,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result2)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -86,6 +97,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result3)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -94,6 +106,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result4)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -103,54 +116,32 @@
 							}
 						}
 					}
-							?>
-			</div>
-			<div class="login">
-				<?php
-					if((!isset($_SESSION['Logged_in'])) or $_SESSION['Logged_in'] != 1){
-					echo "Not logged in";
-					echo "<a class='one' href='login.php'>Login</a>";
-					}
-					else {
-					echo "Logged In: ".$_SESSION['Username'];
-					echo "<a class='one' href='process_logout.php'>Logout</a>";
-					
-						$username = $_SESSION['Username'];
-						
-					$user_rank_query = "SELECT * FROM Users WHERE Username = '$username'";
-					$user_rank_result = mysqli_query($conn, $user_rank_query);
-					$user_rank_row = mysqli_fetch_assoc($user_rank_result);
-
-					$user_rank = $user_rank_row['Rank']; //store the users rank as a variable
-					$required_rank = "admin"; 
-						
-						if($user_rank == $required_rank){
-							echo "<a class='one' href='add_page.php'>Admin</a>";
-						}	
-					}
 				?>
 			</div>
+			<!-- Link to login page -->
+			<div class="login">
+				<a class='one' href='login.php'>Login</a>
+			</div>
+			<!-- Nav bar links to all of the pages -->
 			<nav>
 				<a class="one" href="home.php">Home</a>
 				<a class="one" href="driver.php">Drivers</a>
 				<a class="one" href="teams.php"> Teams</a>
 				<a class="one" href="tracks.php"> Tracks</a>
-				<a class="one" href="race.php"> Races</a>
     			<link rel="stylesheet" type="text/css" href="styles.css">
 			</nav>
 		</header>
 	<?php
-	
+		// query to delete track
 		$delete_track = "DELETE FROM Track WHERE TrackID='$_GET[TrackID]'";
 		
-	
+		// checks to see if track deletion failed
 		if(!mysqli_query($conn, $delete_track)) {
 			echo 'Track not deleted: '.mysqli_error($conn);
 			header("refresh:20, url = add_page.php");
 		} else {
+			// if track deletion worked
 			echo 'Track deleted';
 			header("refresh:3, url = add_page.php");
 		}
-	
-	
 	?>	
