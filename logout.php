956607cd829b4a6917682dc4e25ca747e6e89e0a
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-10-02 00:30:43 +1300

    commenting and code validation

--- logout.php
+++ logout.php
@@ -1,27 +1,35 @@
+<!DOCTYPE html>
+<HTML lang="en">
 <?php
+/*starts the session and has databse connection so queries work*/
 	session_start();
 	include '../f1_mysqli.php';
 ?>
-<html>
 	<head>
+		<!-- Link to style sheet  -->
 		<link rel="stylesheet" href="styles.css">
+		<meta charset="utf-8">
+		<!-- Creates title -->
 		<title>
-			Formula 1 - Home
+			Formula 1 - Logout
 		</title>
 	</head>
-
 	<body>
 		<header> 
 			<div class="logo">
-				<a class="two" href="home.php">
+				<!--Image acts as a link to the home page -->
+				<a class="two" href="home.ph">
             		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
 				</a>
 			</div>
+			<!-- Is the header section  -->
 			<h1 class="title">
-				f1 - home
+				Formula 1 - Logout
 			</h1>
+			<!-- Search bar section -->
 			<div class="search">
 				<h1>Search</h1>
+				<!-- Starts the search query -->
 				<form method="post">
 					<input type="text" name="search">
 					<input type="submit" name="submit" value="Search" class="search_button">
@@ -30,32 +38,35 @@
 					/* searches database to see if the input matches */
 					if (isset($_POST['search'])) {
 						$search = $_POST['search'];
-
+						/*Query based on search like first name*/
 						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Fname LIKE '%$search%'";
 						
+						/*Query based on search like last name*/
 						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Lname LIKE '%$search%'";
 						
+						/*Query based on search like driver number*/
 						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND DriverID LIKE '%$search%'";
 						
+						/*Query based on seach like team name*/
 						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND TeamName LIKE '%$search%'";
 						
-
+						/*turns queries into reusults*/
 						$search_query_result1 = mysqli_query($conn, $search_query_fname);
 						$search_query_result2 = mysqli_query($conn, $search_query_lname);
 						$search_query_result3 = mysqli_query($conn, $search_query_DriverID);
@@ -77,6 +88,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result2)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -85,6 +97,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result3)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -93,6 +106,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result4)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -103,10 +117,36 @@
 						}
 					}
 							?>
+			
 			</div>
+			<!-- Link to login page -->
 			<div class="login">
-				<a class="one" href="login.php">Login</a>
+				<?php
+					//checks to see if logged in
+					if((!isset($_SESSION['Logged_in'])) or $_SESSION['Logged_in'] != 1){
+					echo "Not logged in";
+					echo "<a class='one' href='login.php'>Login</a>"; // displays if not logged in
+					}
+					else {
+					echo "Logged In: ".$_SESSION['Username']; // shows username of who is logged in
+					echo "<a class='one' href='process_logout.php'>Logout</a>"; // dislpayed if loffed in
+					
+						$username = $_SESSION['Username'];
+						
+					$user_rank_query = "SELECT * FROM Users WHERE Username = '$username'";
+					$user_rank_result = mysqli_query($conn, $user_rank_query);
+					$user_rank_row = mysqli_fetch_assoc($user_rank_result);
+
+					$user_rank = $user_rank_row['Rank']; //store the users rank as a variable
+					$required_rank = "admin"; 
+						
+						if($user_rank == $required_rank){
+							echo "<a class='one' href='add_page.php'>Admin</a>"; // hsows admin tag if logged in
+						}	
+					}
+				?>
 			</div>
+			<!-- Nav bar links to all of the pages -->
 			<nav>
 				<a class="one" href="home.php">Home</a>
 				<a class="one" href="driver.php">Drivers</a>
