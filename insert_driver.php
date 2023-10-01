1db5b328f0fb4a06823c51b4bb7761070172cc89
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-10-02 00:02:07 +1300

    commenting and code validation

--- insert_driver.php
+++ insert_driver.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
+<HTML lang="en">
 <?php
+/*starts the session and has databse connection so queries work*/
 	session_start();
 	include '../f1_mysqli.php';
 ?>
-<html>
 	<head>
+		<!-- Link to style sheet  -->
 		<link rel="stylesheet" href="styles.css">
+		<meta charset="utf-8">
+		<!-- Creates title -->
 		<title>
-			Formula 1 Add
+			Formula 1 - Add
 		</title>
 	</head>
-
 	<body>
 		<header> 
 			<div class="logo">
-				<a class="two" href="home.php">
+				<!--Image acts as a link to the home page -->
+				<a class="two" href="home.ph">
             		<img src="Images/F1-logo.png" alt="F1 logo" height="150" width="270">	
 				</a>
 			</div>
+			<!-- Is the header section  -->
 			<h1 class="title">
-				Formula 1 - add
+				Formula 1 - Add
 			</h1>
+			<!-- Search bar section -->
 			<div class="search">
 				<h1>Search</h1>
+				<!-- Starts the search query -->
 				<form method="post">
 					<input type="text" name="search">
 					<input type="submit" name="submit" value="Search" class="search_button">
@@ -31,32 +38,35 @@
 					/* searches database to see if the input matches */
 					if (isset($_POST['search'])) {
 						$search = $_POST['search'];
-
+						/*Query based on search like first name*/
 						$search_query_fname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Fname LIKE '%$search%'";
 						
+						/*Query based on search like last name*/
 						$search_query_lname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND Lname LIKE '%$search%'";
 						
+						/*Query based on search like driver number*/
 						$search_query_DriverID = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND DriverID LIKE '%$search%'";
 						
+						/*Query based on seach like team name*/
 						$search_query_teamname = "SELECT DISTINCT b.Fname, b.Lname, d.DriverID, t.TeamName
 											  FROM Bio b, Driver d, Team t
 											  WHERE b.BioID = d.BioID
 											  AND t.TeamID = d.TeamID
 											  AND TeamName LIKE '%$search%'";
 						
-
+						/*turns queries into reusults*/
 						$search_query_result1 = mysqli_query($conn, $search_query_fname);
 						$search_query_result2 = mysqli_query($conn, $search_query_lname);
 						$search_query_result3 = mysqli_query($conn, $search_query_DriverID);
@@ -78,6 +88,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result2)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -86,6 +97,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result3)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -94,6 +106,7 @@
 								echo "<br>";
 							}
 						while ($row = mysqli_fetch_array($search_query_result4)) {
+							/* prints search results */
 								echo "Driver and number: ";
 								echo $row['Fname']." ".$row['Lname']." - ".$row['DriverID'];
 								echo "<br>";
@@ -104,32 +117,13 @@
 						}
 					}
 							?>
+			
 			</div>
+			<!-- Link to login page -->
 			<div class="login">
-				<?php
-					if((!isset($_SESSION['Logged_in'])) or $_SESSION['Logged_in'] != 1){
-					echo "Not logged in";
-					echo "<a class='one' href='login.php'>Login</a>";
-					}
-					else {
-					echo "Logged In: ".$_SESSION['Username'];
-					echo "<a class='one' href='process_logout.php'>Logout</a>";
-					
-						$username = $_SESSION['Username'];
-						
-					$user_rank_query = "SELECT * FROM Users WHERE Username = '$username'";
-					$user_rank_result = mysqli_query($conn, $user_rank_query);
-					$user_rank_row = mysqli_fetch_assoc($user_rank_result);
-
-					$user_rank = $user_rank_row['Rank']; //store the users rank as a variable
-					$required_rank = "admin"; 
-						
-						if($user_rank == $required_rank){
-							echo "<a class='one' href='add_page.php'>Admin</a>";
-						}	
-					}
-				?>
+				<a class='one' href='login.php'>Login</a>
 			</div>
+			<!-- Nav bar links to all of the pages -->
 			<nav>
 				<a class="one" href="home.php">Home</a>
 				<a class="one" href="driver.php">Drivers</a>
@@ -151,38 +145,47 @@
 		$Champs = $_POST['Championships'];
 		$Image = $_POST['Image'];
 		
-		
+		// query to insert into the bio table
 		$insert_bio = "INSERT INTO Bio (Fname, Lname, DOB, Nationality, Description) VALUES ('$Fname', '$Lname', '$DOB', '$Nation', '$Desc') ";
 		
 		
 		// Execute the first query
 		if (!mysqli_query($conn, $insert_bio)) {
+			//checks is insertion is failed an dgives reason
 			echo 'Bio insertion failed: ' . mysqli_error($conn);
 		} else {
+			// if bio insertion is sucessful
 			echo 'Bio added';
 		}
 
-		// Execute the second query
+		// query to get bioid based on f and l name
 		$driver_query = "SELECT BioID FROM Bio WHERE Fname = '$Fname' AND Lname = '$Lname'";
 		$driver_result = mysqli_query($conn, $driver_query);
 
+		// checks if query fails
 		if (!$driver_result) {
-			echo 'Driver query failed: ' . mysqli_error($conn);
+			echo 'Driver query failed: '. mysqli_error($conn);
 		} else {
+			// if query works it carries on
 			$driver_record = mysqli_fetch_assoc($driver_result);
 
+			// checks to see if it fails
 			if (!$driver_record) {
 				echo 'No BioID found';
 			} else {
+				// gets bioid
 				$BioID = $driver_record['BioID'];
 
+				// query to insert into driver table
 				$insert_driver = "INSERT INTO Driver (DriverID, BioID, TeamID, Poles, Wins, Championships, Image) VALUES ('$DriverID', '$BioID', '$TeamID', '$Poles', '$Wins', '$Champs', '$Image')";
 
+				// checks to see if it fails
 				if (!mysqli_query($conn, $insert_driver)) {
 					echo 'Driver insertion failed: ' . mysqli_error($conn);
 				} else {
+					// if driver insertion works
 					echo 'Driver added';
-					header("refresh:7, url=driver.php");
+					header("refresh:3, url=add_page.php");
 				}
 			}
 		}
